9d281e40e34560d06e441f03cc6976e758a36b88
Rachel Braband <rebraband@gmail.com>
2020-08-18 08:43:16 -0500

    commit on 'master'

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 'use strict';
 const express = require('express');
+const bcryptjs = require('bcryptjs');
 const router = express.Router();
 const User = require('./models').User;
+const { check, validationResult } = require('express-validator');
 
 //function to wrap each route in try/catch blocks. Saves time and coding space
 function asyncHandler(cb) {
@@ -13,20 +15,41 @@ function asyncHandler(cb) {
           }
     }
 }
+
+
+
+const nameValidator = check('name')
+  .exists({ checkNull: true, checkFalsy: true })
+  .withMessage('Please provide a value for "name"');
+
 /*****USER ROUTES******/
 const users = [];
 /*****Returns the currently authenticated user STATUS: 200 *****/
 router.get('/users', asyncHandler,(async(req, res) => {
-    // const user = await User.findAll({
-
-    // });
     res.json(users);
 }));
 
 /***** Creates a user, sets Location header to '/' and returns no content STATUS: 201 *****/
-router.post('/users', (req, res) => {
+router.post('/users', [
+    check('name')
+        .exists()
+        .withMessage('Please provide a value for "name"'),
+    check('username')
+        .exists()
+        .withMessage('Please provide a value for "username"'),
+    check('password')
+        .exists()
+        .withMessage('Please provide a value for "password"'),
+    ], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const errorMessages = errors.array().map(error => error.msg);
+        return res.status(400).json({ errors: errorMessages });
+    }
     //Get the user from the request body
     const user = req.body;
+    // Hash the new user's password.
+    user.password = bcryptjs.hashSync(user.password);
     //Add the user to the 'users' array
     users.push(user);
     //Set the status to 201, created and end the response
